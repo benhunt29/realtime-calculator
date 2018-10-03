d6af6a3a9fad7d9f70f13c8f1b8213b478a54f39
benhunt29 <benhunt29@gmail.com>
2018-10-02 20:50:44 -0500

    use deployed url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const PUSHER_APP_KEY = '9280c64979af1455feae'
 export const PUSHER_CLUSTER = 'us2'
-export const API_BASE_URL = 'http://localhost:3001' // 'https://realtime-calculator-api-bhunt.herokuapp.com'
+export const API_BASE_URL = 'https://realtime-calculator-api-bhunt.herokuapp.com'
